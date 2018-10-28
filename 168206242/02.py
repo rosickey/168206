43d569df9b6d69bffe829a42eb70c7e058a891e2
bigbenzz <44491996+bigbenzz@users.noreply.github.com>
2018-10-28 15:06:17 +0800

    Create 02.py

---
+++ 168206242/02.py
@@ -0,0 +1,19 @@
+
+def findbig(list):
+    big=list[0]
+    big_index=0
+    for i in range(1,len(list)):
+        if list[i]>big:
+            big=list[i]
+            big_index=i
+    return big_index
+
+def selectbig(list2):
+    newbig=[]
+    for i in range(len(list2)):
+        big=findbig(list2)
+        newbig.append(list2.pop(big))
+    return newbig
+
+L=[6,4,8,0,3,5,2]
+print(sum(L))
