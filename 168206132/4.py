21ee4de1a739855c40832b67b8f961e069985213
silenceypx <44491644+silenceypx@users.noreply.github.com>
2018-10-29 15:12:12 +0800

    Create 4.py

---
+++ 168206132/4.py
@@ -0,0 +1,25 @@
+def QuickSort(arr,firstIndex,lastIndex):
+    if firstIndex<lastIndex:
+        divIndex=Partition(arr,firstIndex,lastIndex)
+ 
+        QuickSort(arr,firstIndex,divIndex)       
+        QuickSort(arr,divIndex+1,lastIndex)
+    else:
+        return
+ 
+ 
+def Partition(arr,firstIndex,lastIndex):
+    i=firstIndex-1
+    for j in range(firstIndex,lastIndex):
+        if arr[j]<=arr[lastIndex]:
+            i=i+1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[lastIndex]=arr[lastIndex],arr[i+1]
+    return i
+ 
+ 
+arr=[1,4,7,1,5,5,3,85,34,75,23,75,2,0]
+ 
+print("initial array:\n",arr)
+QuickSort(arr,0,len(arr)-1)
+print("result array:\n",arr)
