bad3a7d2485e0258d08c9448e01caed26246689a
Tjp0813 <44577604+Tjp0813@users.noreply.github.com>
2018-10-29 22:18:34 +0800

    Create 归并排序.py

---
+++ 168206125/归并排序.py
@@ -0,0 +1,22 @@
+def MergeSort(lists):
+    if len(lists) <= 1:
+        return lists
+    num = int( len(lists) / 2 )
+    left = MergeSort(lists[:num])
+    right = MergeSort(lists[num:])
+    return Merge(left, right)
+def Merge(left,right):
+    r, l=0, 0
+    result=[]
+    while l<len(left) and r<len(right):
+        if left[l] < right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+    result += left[l:]
+    result += right[r:]
+    return result
+lists=[1, 2, 3, 4, 5, 6, 7, 90, 21, 23, 45]
+print(MergeSort(lists))
