2e461666658833217d058164c9157d84bff77f0b
tianhahaha <44495587+tianhahaha@users.noreply.github.com>
2018-10-30 00:37:45 +0800

    Create 02 binary_search_1.py

---
+++ 168206201/02 binary_search_1.py
@@ -0,0 +1,27 @@
+#二分查找法的递归算法
+def binary_search_1(L,number,low,high):
+    
+    
+    if low <= high:
+        mid = (high + low)//2
+        guess = L[mid]
+        print('number',number,'guess',guess,'mid',mid)
+        if guess == number:
+            print('哈哈')
+            print(mid,guess)
+            return mid,guess
+        elif guess > number:
+            return binary_search_1(L,number,low,mid-1)
+        elif guess < number:
+            return binary_search_1(L,number,mid+1,high)
+    else: 
+        return
+    
+if __name__ =='__main__' :
+    while True:
+        number = int(input('你输入你想要的数字:'))
+        index,num= binary_search_1(L,number,0,len(L)-1)
+        print('位置在',index,'数字是',num)
+        c = input('是否想要退出？')
+        if c == 'Y':
+            break
