0c2640122d3c30c5efcefac282b70ae37818381a
blinkycc <44491634+blinkycc@users.noreply.github.com>
2018-10-30 19:22:31 +0800

    合并排序.py

---
+++ 168206235/05.py
@@ -0,0 +1,28 @@
+def merge(a, b):
+    c = []
+    h = j = 0
+    while j < len(a) and h < len(b):
+        if a[j] < b[h]:
+            c.append(a[j])
+            j += 1
+        else:
+            c.append(b[h])
+            h += 1
+
+    if j == len(a):
+        for i in b[h:]:
+            c.append(i)
+    else:
+        for i in a[j:]:
+            c.append(i)
+    return c
+def merge_sort(lists):
+    if len(lists) <= 1:
+        return lists
+    middle = len(lists)//2
+    left = merge_sort(lists[:middle])
+    right = merge_sort(lists[middle:])
+    return merge(left, right)
+if __name__ == '__main__':
+    a = [4, 7, 8, 3, 5, 9]
+    print (merge_sort(a))
