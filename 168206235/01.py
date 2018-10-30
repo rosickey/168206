4a4aba5c8392b32b172a707f74863ab0b0514c66
blinkycc <44491634+blinkycc@users.noreply.github.com>
2018-10-30 19:12:31 +0800

    递归二分法查找.py

---
+++ 168206235/01.py
@@ -0,0 +1,16 @@
+def seach(seq ,number  ):
+    lower=0
+    upper=len(seq )-1     
+    mid=(seq [lower]+seq [upper])//2
+    if number==seq [mid]:
+        return mid
+    elif number>seq [mid]:
+        lower=mid+1
+        return seach(seq , number)
+    elif number<seq [mid]:
+        upper=mid
+        return seach(seq , number)
+    else:
+        return False
+seq=range(0,100)
+print(seach(seq ,33))
