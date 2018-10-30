248963b79eb1b8a99ecff7548fdd203191ccef62
lyndonMurphy <44491689+lyndonMurphy@users.noreply.github.com>
2018-10-30 17:43:52 +0800

    Update 选择排序.py

--- 168206107/选择排序.py
+++ 168206107/选择排序.py
@@ -11,3 +11,18 @@ def selectsort(alist):
     
 alist = [1, 2, 3, 4, 5, 6, 7, 90, 21, 23, 45]
 print(selectsort(alist))
+
+#another way
+"""
+def selectsort(alist):
+    thelist = []
+    while alist:
+        for index in range(1,len(alist)):
+            if alist[0] >= alist[index]:
+                alist[0],alist[index]=alist[index],alist[0]
+        thelist.append(alist.pop(0))
+    return thelist
+
+a = [5,6,8,89,7,4,5,84]
+print(selectsort(a)) 
+""""
