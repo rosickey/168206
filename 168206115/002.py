3d8fc2d466ecbc9af2aa20b91c32923a1dfdfe3b
lihuixiang <44491961+lihuixiang@users.noreply.github.com>
2018-11-21 16:43:09 +0800

    Create 002.py

---
+++ 168206115/002.py
@@ -0,0 +1,20 @@
+def    fd(arg1 , arg2 , max)
+       if  arg1==0:
+       print(arg1)
+       print(arg2)
+     arg3==arg1+arg2
+      print(arg3)
+      if  arg3<max:
+          fd(arg2 arg3,max)
+          
+          
+          
+      例如：    fd(0,2,10)
+          
+          #结果： 
+          0
+          2
+          2
+          4
+          6
+          10
